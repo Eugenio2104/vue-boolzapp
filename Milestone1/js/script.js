9480c79fba6a38dcc7d42eb5dfc1445ec101cb5e
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-11-07 18:30:33 +0100

    reinserimento vue

--- Milestone1/js/script.js
+++ Milestone1/js/script.js
@@ -1,4 +1,4 @@
-const { createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
   data(){
@@ -167,6 +167,8 @@ createApp({
         }
         ]
     }
-  }
+  },
+  mounted(){
+    console.log('montata');
+  },
 }).mount('#app')
-
